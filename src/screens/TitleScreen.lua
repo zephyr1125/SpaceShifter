f1d50fd257d5176e5b1876ecacd5d2816f4f3b4b
Zephyr Zhang <zephyr1125@gmail.com>
2019-08-27 21:58:14 +0800

    首页选项改为英文,以避免可能的问题

--- src/screens/TitleScreen.lua
+++ src/screens/TitleScreen.lua
@@ -12,11 +12,11 @@ function TitleScreen:init(ScreenManager)
 	self.screen = ScreenManager
 	
 	self.buttons = SelectionGroup()
-	self.startButton = Button('开始游戏', buttonWidth, buttonHeight,
+	self.startButton = Button('Start', buttonWidth, buttonHeight,
 			buttonIdleColor, buttonSelectColor, function()
 				self.screen:view('game')
 			end)
-	self.exitButton = Button('退出游戏', buttonWidth, buttonHeight,
+	self.exitButton = Button('Exit', buttonWidth, buttonHeight,
 			buttonIdleColor, buttonSelectColor, function ()
 				love.event.quit()
 			end)
